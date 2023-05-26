13c1327e60c13bfff5b3650e0938e087c99f3721
AlexanderRama <90639823+AlexanderRama@users.noreply.github.com>
2023-05-26 19:20:13 +0700

    OTP

--- app/src/main/java/com/example/mobile_dev/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mobile_dev/ui/auth/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.mobile_dev.ui.auth
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.mobile_dev.R
+import com.example.mobile_dev.databinding.ActivityLoginBinding
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.regist.setOnClickListener{
+            val i = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(i)
+        }
     }
 }
