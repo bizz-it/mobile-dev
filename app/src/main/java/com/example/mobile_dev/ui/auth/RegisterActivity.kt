13e733c51b44cf858fb6b8f5fc2fab7df8b2e846
AlexanderRama <90639823+AlexanderRama@users.noreply.github.com>
2023-05-26 19:34:15 +0700

    Update RegisterActivity.kt

--- app/src/main/java/com/example/mobile_dev/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/mobile_dev/ui/auth/RegisterActivity.kt
@@ -7,6 +7,16 @@ import com.example.mobile_dev.R
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_register)
+=======
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Okeee
+        binding.login.setOnClickListener{
+            val i = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(i)
+        }
+>>>>>>> Stashed changes
     }
 }
