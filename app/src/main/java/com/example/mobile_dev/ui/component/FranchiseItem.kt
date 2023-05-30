d882a7e9988e1af26a312863515ab6d159022542
Clint02 <81024079+Clint02@users.noreply.github.com>
2023-05-31 00:29:35 +0700

    Ken (#6)
    * tes 1
    
    * tes 2
    
    * update gradle, error :)
    
    * Test Graddle
    
    * on boarding, home, bottom navbar 50%
    
    * component franchise item and searchbar
    
    ---------
    
    Co-authored-by: AlexanderRama <90639823+AlexanderRama@users.noreply.github.com>

--- app/src/main/java/com/example/mobile_dev/ui/component/FranchiseItem.kt
+++ app/src/main/java/com/example/mobile_dev/ui/component/FranchiseItem.kt
@@ -1,89 +1,121 @@
 package com.example.mobile_dev.ui.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mobile_dev.R
-import com.example.mobile_dev.ui.theme.Shapes
+import com.example.mobile_dev.ui.theme.LightBlue
+import com.example.mobile_dev.ui.theme.MobiledevTheme
 
 @Composable
 fun FranchiseItem(
     image: Int,
     title: String,
-    rating: Float,
+    price: String,
+    totalShop: Int,
+    category: Int,
     modifier: Modifier = Modifier,
 ) {
-    val ratingString = rating.toString()
-    Box(modifier = modifier) {
+    Row(
+        modifier = modifier
+            .width(361.dp)
+            .height(122.dp)
+            .fillMaxWidth()
+            .border(2.dp, LightBlue, RoundedCornerShape(12.dp)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = painterResource(image),
             contentDescription = null,
             contentScale = ContentScale.Fit,
             modifier = Modifier
                 .width(120.dp)
-                .height(160.dp)
-                .clip(Shapes.medium)
+                .height(120.dp)
         )
-        Box(
-            modifier = modifier.align(Alignment.BottomCenter)
+        Column(
+            modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 8.dp)
         ) {
-            Image(
-                painter = painterResource(R.drawable.cart_bg),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .width(120.dp)
-                    .height(50.dp)
-                    .clip(Shapes.medium)
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
             )
-            Column(
-                modifier = Modifier.align(Alignment.Center),
-                horizontalAlignment = Alignment.CenterHorizontally
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = title,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.subtitle1.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                )
-                Row(modifier = Modifier) {
-                    Icon(
-                        imageVector = Icons.Default.Star,
-                        contentDescription = null,
-                        tint = Color.Yellow
-                    )
-                    Spacer(
-                        modifier = Modifier.width(8.dp)
+                Column(
+                    modifier = Modifier
+                ) {
+                    Text(
+                        text = "Start From",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 8.sp,
                     )
                     Text(
-                        text = ratingString,
-                        style = MaterialTheme.typography.subtitle2,
-                        color = MaterialTheme.colors.secondary
+                        text = stringResource(R.string.idr) + " " + price,
+                        fontWeight = FontWeight.Bold,
+                        color = LightBlue,
+                        fontSize = 20.sp,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(R.string.total_shop) + " " + totalShop + " " + stringResource(
+                            R.string.shop
+                        ),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 12.sp,
                     )
                 }
+                Image(
+                    painter = painterResource(category),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .width(24.dp)
+                        .height(24.dp)
+                        .align(Alignment.Bottom)
+                        .weight(1f, false)
+                )
+
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FranchiseItemPreview() {
+    MobiledevTheme {
+        FranchiseItem(
+            R.drawable.logo_mixue,
+            "Mixue Ice Cream & Teaa",
+            "3,5 Mil",
+            1800,
+            R.drawable.fnb_icon
+        )
     }
 }
